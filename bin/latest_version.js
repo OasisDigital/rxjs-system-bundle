61975615ca209661a26370ddfc3acd3605453c3d
Alex Gyoshev <alexander.gyoshev@gmail.com>
2017-09-14 16:25:31 +0300

    Add Travis build for publishing new versions (#3)
    * Create bundle only after successful version increases
    
    * Succeed if the latest version is already published
    
    * Use Travis NPM deploy provider
    
    see https://docs.travis-ci.com/user/deployment/npm/

---
+++ bin/latest_version.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const api = require('api-npm');
+
+const packageFileName = '../package.json';
+const package = require(packageFileName);
+
+const matchVersion = require('./match_version');
+
+const latest = package => package['dist-tags'].latest;
+
+api.getdetails('rxjs', rxjs => {
+  api.getdetails(package.name, systemBundle => {
+
+    if (latest(rxjs) == latest(systemBundle)) {
+      console.error("At most recent version");
+      process.exit(0);
+    }
+
+    const lastVersion = latest(rxjs);
+    console.log("Writing package.json with latest version", lastVersion);
+    package.devDependencies.rxjs = lastVersion;
+    fs.writeFileSync(packageFileName, JSON.stringify(package, null, 2) + '\n');
+
+    matchVersion(err => {
+      if (err) {
+        console.error(err);
+        process.exit(60);
+      }
+    });
+  });
+});
