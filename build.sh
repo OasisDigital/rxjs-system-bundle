314b94411c563e03ea9a9df1be66c81f08cc2481
Kyle Cordes <kyle.cordes@oasisdigital.com>
2017-11-03 04:27:29 -0500

    operators.js fixed in core RxJS, workaround won't be needed
    ... in next RxJS release.

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ node bin/latest_version.js
 RESULT=$(cat RESULT)
 npm install
 npm update
-sed -e "s/'\./'.\/operators/" <node_modules/rxjs/operators/index.js >node_modules/rxjs/operators.js
+# Former workaround for missing operators.js file
+# sed -e "s/'\./'.\/operators/" <node_modules/rxjs/operators/index.js >node_modules/rxjs/operators.js
 node bin/make_bundle.js
