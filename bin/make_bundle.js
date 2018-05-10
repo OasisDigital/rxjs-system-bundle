2adf17f2421f4551857da73b34b1ae18ad9ffca4
Lakmal Molligoda <mlakmal@gmail.com>
2018-05-10 13:40:26 -0400

    fix js lint issue

--- bin/make_bundle.js
+++ bin/make_bundle.js
@@ -1,34 +1,34 @@
-const Builder = require("systemjs-builder");
-const matchVersion = require("./match_version");
+const Builder = require('systemjs-builder');
+const matchVersion = require('./match_version');
 
 function bundle() {
-  const builder = new Builder("./");
+  const builder = new Builder('./');
   builder.config({
     map: {
-      rxjs: "node_modules/rxjs"
+      rxjs: 'node_modules/rxjs'
     },
     packages: {
       rxjs: {
-        main: "index.js",
-        defaultExtension: "js"
+        main: 'index.js',
+        defaultExtension: 'js'
       },
-      "rxjs/operators": {
-        main: "index.js",
-        defaultExtension: "js"
+      'rxjs/operators': {
+        main: 'index.js',
+        defaultExtension: 'js'
       },
-      "rxjs/Observable": {
-        defaultExtension: "js"
+      'rxjs/Observable': {
+        defaultExtension: 'js'
       }
     }
   });
 
-  console.log("Bundling Rx");
+  console.log('Bundling Rx');
   return builder
-    .bundle("rxjs + rxjs/operators", "Rx.system.js", {
+    .bundle('rxjs + rxjs/operators', 'Rx.system.js', {
       sourceMaps: true
     })
     .then(ignored => {
-      builder.bundle("rxjs + rxjs/operators", "Rx.system.min.js", {
+      builder.bundle('rxjs + rxjs/operators', 'Rx.system.min.js', {
         sourceMaps: true,
         minify: true
       });
